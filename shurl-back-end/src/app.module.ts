666db8649a78584c9316027efd3c4305bcf23104
NEONKID <clax1412@outlook.com>
2020-02-11 14:46:44 +0900

    Add dotenv, ConfigService and modules...

--- shurl-back-end/src/app.module.ts
+++ shurl-back-end/src/app.module.ts
@@ -3,10 +3,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UrlModule } from './url/url.module';
+import { UrlModule } from './modules/url.module';
+import { ConfigModule } from './modules/config.module';
 
 @Module({
-    imports: [TypeOrmModule.forRoot(), UrlModule],
+    imports: [TypeOrmModule.forRoot(), ConfigModule, UrlModule],
     controllers: [AppController],
     providers: [AppService],
 })
