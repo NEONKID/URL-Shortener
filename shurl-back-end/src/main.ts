ab0d6de523b3f19f025288fc0d6c2cbe90959f1b
NEONKID <clax1412@outlook.com>
2020-02-26 21:37:24 +0900

    Add CORS origin...

--- shurl-back-end/src/main.ts
+++ shurl-back-end/src/main.ts
@@ -1,13 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import * as dotenv from 'dotenv';
 
-import { AppModule } from './app.module';
+import { AppModule } from './modules/app.module';
 import { dotEnvOptions } from './config/dotenv.options';
+
 dotenv.config(dotEnvOptions);
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
-    app.setGlobalPrefix('/api');
+    app.enableCors({
+        origin: process.env.MAIN_URL,
+        methods: 'GET, HEAD, PUT, PATCH, POST, DELETE',
+    });
     await app.listen(process.env.LISTEN_PORT || 1412);
 }
 bootstrap();
