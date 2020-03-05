b86d73eb32a2be1e2bde985390ddc05fbbb72090
NEONKID <clax1412@outlook.com>
2020-03-05 18:25:52 +0900

    Remove after code for test code

--- shurl-back-end/test/app.e2e-spec.ts
+++ shurl-back-end/test/app.e2e-spec.ts
@@ -19,16 +19,16 @@ beforeAll(async () => {
     await app.init();
 });
 
-afterAll(async done => {
-    await repo.query(`DROP TABLE url;`);
-    await app.close();
+// afterAll(async done => {
+//     await repo.query(`DROP TABLE url;`);
+//     await app.close();
 
-    done();
-});
+//     done();
+// });
 
 describe('GET /', () => {
-    it('Main Endpoint test', async done => {
-        await request(app.getHttpServer())
+    it('Main Endpoint test', done => {
+        return request(app.getHttpServer())
             .get('/')
             .expect(301)
             .end((err, res) => {
@@ -39,8 +39,8 @@ describe('GET /', () => {
 });
 
 describe('GET, POST /api/url', () => {
-    it('URL Endpoint test', async done => {
-        await request(app.getHttpServer())
+    it('URL Endpoint test', done => {
+        return request(app.getHttpServer())
             .get('/api/url')
             .expect(200)
             .end((err, res) => {
@@ -50,8 +50,8 @@ describe('GET, POST /api/url', () => {
             });
     });
 
-    it('URL Register Test', async done => {
-        await request(app.getHttpServer())
+    it('URL Register Test', done => {
+        return request(app.getHttpServer())
             .post('/api/url/register')
             .send({ url: 'https://neonkid.xyz' })
             .expect('Content-Type', /json/)
@@ -64,8 +64,8 @@ describe('GET, POST /api/url', () => {
 });
 
 describe('Shortcut Endpoint Test', () => {
-    it('URL Redirect Test', async done => {
-        await request(app.getHttpServer())
+    it('URL Redirect Test', done => {
+        return request(app.getHttpServer())
             .get('/1')
             .expect(302)
             .end((err, res) => {
