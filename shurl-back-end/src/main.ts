930c552ffbe0ccd86fd3957ec36f690480050e07
Kwang Soo Jeong <clax1412@outlook.com>
2020-03-13 11:18:16 +0900

    How to run on machine guidelines.. (#15)
    * Remove previous README..
    
    * Add Back-end SSL options..
    
    * Add LICENSE..
    
    * Add Guidelines..
    
    * Fix test environment configuration..

--- shurl-back-end/src/main.ts
+++ shurl-back-end/src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import * as dotenv from 'dotenv';
+import * as fs from 'fs';
 
 import { AppModule } from './modules/app.module';
 import { dotEnvOptions } from './config/dotenv.options';
@@ -7,11 +8,20 @@ import { dotEnvOptions } from './config/dotenv.options';
 dotenv.config(dotEnvOptions);
 
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule);
+    const httpsOptions = {
+        key: fs.readFileSync(process.env.SSL_KEY_FILE),
+        cert: fs.readFileSync(process.env.SSL_CERT_FILE),
+    };
+
+    const app = await NestFactory.create(AppModule, {
+        httpsOptions,
+    });
+
     app.enableCors({
         origin: process.env.MAIN_URL,
         methods: 'GET, HEAD, PUT, PATCH, POST, DELETE',
     });
+
     await app.listen(process.env.LISTEN_PORT || 1412);
 }
 bootstrap();
--- shurl-front-end/README.md
+++
@@ -1,3 +0,0 @@
-# URL Shortener Front-end
-
-URL Shortener Front-end Source directory..
